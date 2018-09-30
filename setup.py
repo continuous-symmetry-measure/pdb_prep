f5fcec99c3903edd6808a1ef8e1d54db7ab4bcd2
Sagiv Barhoom <sagivba@gmail.com>
2018-09-30 17:34:12 +0300

    setup test

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup
 
 setup(
-    name='TheoChemPDB',
-    version='0.0.8.1',
-    packages=['_Tests', 'Chemistry','Chemistry.PDB', 'Geometry', 'PDB_Distance','PDB_Prep','Utils'],
+    name='pdb_prep',
+    version='0.0.8.2',
+    packages=['click','_Tests', 'Chemistry','Chemistry.PDB', 'Geometry', 'PDB_Distance','PDB_Prep','Utils'],
     url='',
     author='Sagiv Barhoom',
     author_email='sagivba@gmail.com',
