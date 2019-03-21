48a1c62ca9e5f1d55578268cd9c5be6088eee490
sagivba <30871008+sagivba@users.noreply.github.com>
2019-03-21 16:22:56 +0200

    'click' was moved to install_requires
    'click' was moved to install_requires as suggested by "sbliven"

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from distutils.core import setup
 setup(
     name='pdb_prep',
     version='0.0.8.2',
-    packages=['click','_Tests', 'Chemistry','Chemistry.PDB', 'Geometry', 'PDB_Distance','PDB_Prep','Utils'],
+    packages=['_Tests', 'Chemistry','Chemistry.PDB', 'Geometry', 'PDB_Distance','PDB_Prep','Utils'],
     url='',
+    install_requires=['click'],
     author='Sagiv Barhoom',
     author_email='sagivba@gmail.com',
     description='pdb utils'
